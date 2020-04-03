6fdce542d93dd15124303e7195c175158c0fc48c
Ghani <abdulghani@users.noreply.github.com>
2020-04-03 19:15:23 +0700

    Update context_provider_test.dart

--- test/context_provider_test.dart
+++ test/context_provider_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:context_provider/context_provider.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('test', () {
     expect(true, true);
   });
 }
